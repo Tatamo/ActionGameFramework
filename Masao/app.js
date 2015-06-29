5ed86ac6d0a7b8279f8e20f644a76a1bac2aa86f
Tatamo <nsorder@gmail.com>
2015-06-29 22:41:30 +0900

    use MapGroup

--- Masao/app.js
+++ Masao/app.js
@@ -534,18 +534,21 @@ var Game;
             }
             Stage.prototype.enter = function (sm) {
                 this.player = new Game.Player(sm.game.gamekey, 224, 128, sm.game.assets.image, "pattern");
-                this.sprites = new Game.Group(sm.game.screen);
-                this.sprites.add(this.player);
+                this.creatures = new Game.Group(sm.game.screen);
+                this.blocks = new Game.MapGroup(sm.game.screen, 180, 30);
+                this.creatures.add(this.player);
                 for (var i = 0; i < 8; i++) {
-                    this.sprites.add(new Game.Block1(128 + i * 32, 160, sm.game.assets.image, "pattern"));
+                    this.blocks.add(new Game.Block1(128 + i * 32, 160, sm.game.assets.image, "pattern"));
                 }
             };
             Stage.prototype.update = function (sm) {
                 // 背景色で埋めてみる
                 sm.game.screen.context.fillStyle = "rgb(0,255,255)";
                 sm.game.screen.context.fillRect(0, 0, screen.width, screen.height);
-                this.sprites.update();
-                this.sprites.draw();
+                this.creatures.update();
+                this.blocks.update();
+                this.creatures.draw();
+                this.blocks.draw();
                 if (sm.game.gamekey.isOnDown(80)) {
                     sm.push(new States.Pause(sm)); // ポーズ
                 }
--- Masao/src/states/stage.ts
+++ Masao/src/states/stage.ts
@@ -2,17 +2,18 @@
     export module States {
         export class Stage extends GameState {
             player: Player;
-            sprites: Group;
-            map: MapGroup;
+            creatures: Group;
+            blocks: MapGroup;
             constructor() {
                 super();
             }
             enter(sm: GameStateMachine) {
                 this.player = new Player(sm.game.gamekey, 224, 128, sm.game.assets.image, "pattern");
-                this.sprites = new Group(sm.game.screen);
-                this.sprites.add(this.player);
+                this.creatures = new Group(sm.game.screen);
+                this.blocks = new MapGroup(sm.game.screen, 180, 30);
+                this.creatures.add(this.player);
                 for (var i: number = 0; i < 8; i++) {
-                    this.sprites.add(new Block1(128+i*32, 160, sm.game.assets.image, "pattern"));
+                    this.blocks.add(new Block1(128+i*32, 160, sm.game.assets.image, "pattern"));
                 }
             }
             update(sm: GameStateMachine) {
@@ -20,8 +21,11 @@
                 sm.game.screen.context.fillStyle = "rgb(0,255,255)";
                 sm.game.screen.context.fillRect(0, 0, screen.width, screen.height);
 
-                this.sprites.update();
-                this.sprites.draw();
+                this.creatures.update();
+                this.blocks.update();
+
+                this.creatures.draw();
+                this.blocks.draw();
 
                 if (sm.game.gamekey.isOnDown(80)) { // Pキー
                     sm.push(new Pause(sm)); // ポーズ
