60838de85a1660b967a5d3c73ffa1eec29cea82d
Tatamo <nsorder@gmail.com>
2015-08-20 16:38:21 +0900

    enable to walk in first frame

--- Masao/app.js
+++ Masao/app.js
@@ -777,16 +777,16 @@ var Game;
         };
         Player.prototype.checkInput = function () {
             //if (this.gk.isDown(37) && this.gk.isDown(39)) { } // 左右同時に押されていたらとりあえず何もしないことに
-            if (this.gk.isOnDown(37)) {
-                if (this.counter["able2runningLeft"] > 0) {
+            if (this.gk.isDown(37)) {
+                if (this.gk.isOnDown(37) && this.counter["able2runningLeft"] > 0) {
                     this.moving.replace(new States.PlayerRunningLeft());
                 }
                 else if (!(this.moving.CurrentState() instanceof States.PlayerRunningLeft)) {
                     this.moving.replace(new States.PlayerWalkingLeft());
                 }
             }
-            else if (this.gk.isOnDown(39)) {
-                if (this.counter["able2runningRight"] > 0) {
+            else if (this.gk.isDown(39)) {
+                if (this.gk.isOnDown(39) && this.counter["able2runningRight"] > 0) {
                     this.moving.replace(new States.PlayerRunningRight());
                 }
                 else if (!(this.moving.CurrentState() instanceof States.PlayerRunningRight)) {
